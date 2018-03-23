97d1d18062f53046b44bbd71437416386adc7adb
Florian Probst <florian.probst@ebcont.com>
2018-03-23 13:06:14 +0100

    add content to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 source vars
-wget https://hg.nginx.org/pkg-oss/archive/stable-1.12.zip
+
+yum groupinstall -y 'Development Tools'
+yum install -y epel-release
+yum install -y perl perl-devel perl-ExtUtils-Embed libxslt libxslt-devel libxml2 libxml2-devel gd gd-devel GeoIP GeoIP-devel openldap-devel wget vim git
+
+# nginx 
+wget https://nginx.org/download/nginx-$NGINX_VER.tar.gz && tar xzvf nginx-$NGINX_VER.tar.gz
+# PCRE
+wget https://ftp.pcre.org/pub/pcre/pcre-$PCRE_VER.tar.gz && tar xzvf pcre-$PCRE_VER.tar.gz
+# zlib
+wget https://www.zlib.net/zlib-$ZLIB_VER.tar.gz && tar xzvf zlib-$ZLIB_VER.tar.gz
+# OpenSSL
+wget https://www.openssl.org/source/openssl-$OPENSSL_VER.tar.gz && tar xzvf openssl-$OPENSSL_VER.tar.gz
+
+echo "READY! Run install.sh to compile and install nginx!"
+
